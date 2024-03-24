9c21b2764c93deae56bafcd17466942d03b4e0ab
CJ Combs <cj@eleviamedia.com>
2024-03-24 00:01:57 -0400

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,135 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Panel1 = New Panel()
+        Label2 = New Label()
+        ComboBox1 = New ComboBox()
+        ListView1 = New ListView()
+        Label1 = New Label()
+        TextBox1 = New TextBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        Panel1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Panel1
+        ' 
+        Panel1.Controls.Add(Button2)
+        Panel1.Controls.Add(Label2)
+        Panel1.Controls.Add(ComboBox1)
+        Panel1.Controls.Add(ListView1)
+        Panel1.Controls.Add(Label1)
+        Panel1.Controls.Add(TextBox1)
+        Panel1.Controls.Add(Button1)
+        Panel1.Location = New Point(12, 12)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(596, 284)
+        Panel1.TabIndex = 0
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.ForeColor = Color.White
+        Label2.Location = New Point(327, 7)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(56, 15)
+        Label2.TabIndex = 5
+        Label2.Text = "Duration:"
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Location = New Point(327, 25)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(92, 23)
+        ComboBox1.TabIndex = 4
+        ' 
+        ' ListView1
+        ' 
+        ListView1.Location = New Point(12, 54)
+        ListView1.Name = "ListView1"
+        ListView1.Size = New Size(569, 216)
+        ListView1.TabIndex = 3
+        ListView1.UseCompatibleStateImageBehavior = False
+        ListView1.View = View.Details
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.ForeColor = Color.White
+        Label1.Location = New Point(12, 7)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(279, 15)
+        Label1.TabIndex = 2
+        Label1.Text = "URL To Use (Suggest an image on website not CDN)"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(12, 25)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(309, 23)
+        TextBox1.TabIndex = 1
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(425, 25)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 0
+        Button1.Text = "Test"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(506, 25)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(75, 23)
+        Button2.TabIndex = 6
+        Button2.Text = "Stop Test"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.Gray
+        ClientSize = New Size(620, 304)
+        Controls.Add(Panel1)
+        FormBorderStyle = FormBorderStyle.FixedSingle
+        Name = "Form1"
+        Text = "Network Filter Tester"
+        Panel1.ResumeLayout(False)
+        Panel1.PerformLayout()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents ListView1 As ListView
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button2 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,155 @@
+﻿Imports System.IO
+Imports System.Net.Http
+
+Public Class Form1
+    Private WithEvents downloadTimer As New Timer()
+    Private downloadTimeInMilliseconds As Integer
+    Private httpClient As New HttpClient()
+    Private stopTesting As Boolean = False
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Set up the ComboBox items
+        ComboBox1.Items.AddRange(New String() {"5 Seconds", "10 Seconds", "20 Seconds", "30 Seconds", "1 Minute", "2 Minutes", "5 Minutes"})
+
+        ' Set up the ListView columns
+        ListView1.View = View.Details
+        ListView1.Columns.Add("Status", 100)
+        ListView1.Columns.Add("Time", 100)
+        ListView1.Columns.Add("File Location", 200)
+
+        ' Set up the timer interval based on the selected time in ComboBox
+        downloadTimer.Interval = 1000 ' Default interval is 1 second
+    End Sub
+
+    Private Async Sub DownloadFileAsync(url As String)
+        Try
+            ' Create the directory if it doesn't exist
+            Dim downloadDirectory As String = "C:\NetworkTesterFiles"
+            If Not Directory.Exists(downloadDirectory) Then
+                Directory.CreateDirectory(downloadDirectory)
+            End If
+
+            ' Create a unique file name to avoid conflicts
+            Dim uniqueFileName As String = Guid.NewGuid().ToString()
+            ' Set the file location for downloading
+            Dim fileLocation As String = Path.Combine(downloadDirectory, uniqueFileName)
+
+            ' Start the download and measure the time
+            Dim startTime As DateTime = DateTime.Now
+            Dim response As HttpResponseMessage = Await httpClient.GetAsync(url)
+            Dim endTime As DateTime = DateTime.Now
+            Dim downloadTimeInSeconds As Integer = CInt((endTime - startTime).TotalSeconds)
+
+            ' Check if the download was successful
+            If response.IsSuccessStatusCode Then
+                Using stream As Stream = Await response.Content.ReadAsStreamAsync()
+                    Using fileStream As FileStream = File.Create(fileLocation)
+                        Await stream.CopyToAsync(fileStream)
+                    End Using
+                End Using
+
+                ' Add an item to the ListView
+                Dim item As New ListViewItem("Accessible")
+                item.SubItems.Add(downloadTimeInSeconds.ToString())
+                item.SubItems.Add(fileLocation)
+                item.ForeColor = Color.Green
+                ListView1.Items.Add(item)
+            Else
+                ' Add an item to the ListView indicating the failure
+                Dim item As New ListViewItem("Filtered")
+                item.SubItems.Add(downloadTimeInSeconds.ToString())
+                item.SubItems.Add(fileLocation)
+                item.ForeColor = Color.Red
+                ListView1.Items.Add(item)
+            End If
+        Catch ex As Exception
+            MessageBox.Show("Error occurred during download: " + ex.Message)
+        End Try
+    End Sub
+
+    Private Sub downloadTimer_Tick(sender As Object, e As EventArgs) Handles downloadTimer.Tick
+        ' Check if a URL is entered
+        If Not String.IsNullOrEmpty(TextBox1.Text) Then
+            ' Perform the download and update the ListView
+            DownloadFileAsync(TextBox1.Text)
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' Check if a URL is entered
+        If Not String.IsNullOrEmpty(TextBox1.Text) Then
+            ' Start the testing
+            StartTesting()
+        Else
+            MessageBox.Show("Please enter a URL.")
+        End If
+    End Sub
+
+    Private Sub StartTesting()
+        ' Reset the stopTesting flag
+        stopTesting = False
+
+        ' Disable the necessary controls
+        ComboBox1.Enabled = False
+        TextBox1.Enabled = False
+        Button1.Enabled = False
+        Button2.Enabled = True
+
+        ' Clear the ListView
+        ListView1.Items.Clear()
+
+        ' Perform the initial download
+        DownloadFileAsync(TextBox1.Text)
+
+        ' Set the timer interval based on the selected time in ComboBox
+        Select Case ComboBox1.SelectedItem.ToString()
+            Case "5 Seconds"
+                downloadTimeInMilliseconds = 5000
+            Case "10 Seconds"
+                downloadTimeInMilliseconds = 10000
+            Case "20 Seconds"
+                downloadTimeInMilliseconds = 20000
+            Case "30 Seconds"
+                downloadTimeInMilliseconds = 30000
+            Case "1 Minute"
+                downloadTimeInMilliseconds = 60000
+            Case "2 Minutes"
+                downloadTimeInMilliseconds = 120000
+            Case "5 Minutes"
+                downloadTimeInMilliseconds = 300000
+        End Select
+
+        ' Start the timer for subsequent downloads
+        downloadTimer.Interval = downloadTimeInMilliseconds
+        downloadTimer.Start()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ' Stop the testing
+        stopTesting = True
+
+        ' Stop the timer
+        downloadTimer.Stop()
+
+        ' Enable the necessary controls
+        ComboBox1.Enabled = True
+        TextBox1.Enabled = True
+        Button1.Enabled = True
+        Button2.Enabled = False
+
+        ' Show a message box indicating that testing has stopped
+        MessageBox.Show("Testing has stopped.")
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        ' No need for any code here
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        ' Delete the downloaded files
+        Dim downloadFolder As String = "C:\NetworkTesterFiles"
+        If Directory.Exists(downloadFolder) Then
+            Directory.Delete(downloadFolder, True)
+        End If
+    End Sub
+End Class
